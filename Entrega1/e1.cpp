d763c8949e1518cf560977f2bc777bd485ef7554
carliquev <144807865+carliquev@users.noreply.github.com>
2025-11-29 16:22:59 +0000

    added stuff to e1

--- Entrega1/e1.cpp
+++ Entrega1/e1.cpp
@@ -1,1 +1,78 @@
+#include <iostream>
+using namespace std;
 
+#define P 1
+#define N 2
+#define A 3
+#define B 4
+
+struct aa {
+    int ePot;
+    int classe;
+};
+
+//Calcula afinidade(classe1, classe2)
+int af(int c1, int c2){
+    if (c1 == P){
+        if (c2 == P){
+            return 1;
+        }
+        else if (c2 == N){
+            return 3;
+        }
+        else if (c2 == A){
+            return 1;
+        }
+        else if (c2 == B){
+            return 3;
+        }
+    }
+    else if (c1 == N){
+        if (c2 == P){
+            return 5;
+        }
+        else if (c2 == N){
+            return 1;
+        }
+        else if (c2 == A){
+            return 0;
+        }
+        else if (c2 == B){
+            return 1;
+        }
+    }
+    else if (c1 == A){
+        if (c2 == P){
+            return 0;
+        }
+        else if (c2 == N){
+            return 1;
+        }
+        else if (c2 == A){
+            return 0;
+        }
+        else if (c2 == B){
+            return 4;
+        }
+    }
+    else if (c1 == B){
+        if (c2 == P){
+            return 1;
+        }
+        else if (c2 == N){
+            return 3;
+        }
+        else if (c2 == A){
+            return 2;
+        }
+        else if (c2 == B){
+            return 3;
+        }
+    }
+    return -1;
+}
+
+//Calcula a energia libertada
+int eLib(int ePot, int ePotE, int ePotD, int af){
+
+}

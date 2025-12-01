2b4a3e8da36f196e430182166f98b61517bae237
Carlota Vicente <carlota.vicente@tecnico.ulisboa.pt>
2025-12-01 15:38:57 +0000

    Actually there's no need to do allat

--- Entrega1/e1.cpp
+++ Entrega1/e1.cpp
@@ -106,14 +106,8 @@ tuple<long long, vector<int>> eMax(vector<aA> chain){
             for(int k = i; k<=j; k++){
                 long long e = eLib(chain[k], chain[i-1], chain[j+1]) + dp[i][k-1] + dp[k+1][j];
                 if (e >= maxi) {
-                    //verificar se ordem[i][k-1] não é vazio
-                    if (!ordem[i][k-1].empty()) {
-                        sol = ordem[i][k-1];
-                    }
-                    //verificar se ordem[k+1][j] não é vazio
-                    if (!ordem[k+1][j].empty()) {
-                        sol.insert(sol.end(), (ordem[k+1][j]).begin(), (ordem[k+1][j]).end());
-                    }
+                    sol = ordem[i][k-1];
+                    sol.insert(sol.end(), (ordem[k+1][j]).begin(), (ordem[k+1][j]).end());
                     sol.push_back(k);
 
                     if (e == maxi) {

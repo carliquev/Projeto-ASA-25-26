b893bb21caf781d7256dfa7519d365aae669d28c
Carlota Vicente <carlota.vicente@tecnico.ulisboa.pt>
2025-12-12 21:53:33 +0000

    placeholders

---
+++ Entrega2/e2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+
+void topologicalOrder() {
+
+}
+
+
+void readInputUser() {
+//guardar também adjacentes
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    return 0;
+}

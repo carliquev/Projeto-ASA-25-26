2bd15dcc4be3fff5776ae20a9290a3a6507c0670
Carlota Vicente <carlota.vicente@tecnico.ulisboa.pt>
2025-12-13 22:41:31 +0000

    Cookámos (we think it works)

--- Entrega2/e2.cpp
+++ Entrega2/e2.cpp
@@ -3,12 +3,13 @@
 #include <vector>
 using namespace std;
 
-int getNumCamiao(int numCAB, int numCam) {
-    return 1 + numCAB%numCam;
+int getNumCamiao(int numCamAB, int qtdCam) {
+    return 1 + numCamAB%qtdCam;
 }
 
 vector<int> topologicalOrder(vector<vector<int>> adj) {
     vector<int> list;
+    list.push_back(0);
     queue<int> q;
     int n = adj.size() -1; //-1 pois criamos adj com size +1
     vector<int> indegree(n+1, 0);
@@ -70,12 +71,54 @@ int main() {
 
     vector<vector<int> > adj = readInputUser(numCruzamentos, numCamioes, m1, m2, numLigCruzamentos);
     // printf("%d %d %d %d", numCruzamentos, numCamioes, m1 , m2);
-    vector<int>topOrder = topologicalOrder(adj);
+    vector<int> top = topologicalOrder(adj);
     // for (int num: topOrder) {
     //     cout << num << " ";
     // }
+    vector<vector<int>> dp (numCruzamentos+1, vector<int>(numCruzamentos+1, 0));
+    vector<vector<pair<int, int>>> rotCam (numCamioes+1, vector<pair<int, int>>());
 
-    vector<vector<int>> dp (numCamioes+1, vector<int>(numCamioes+1, 0));
+    for (int i=numCruzamentos; i>0; i--) {
+        for (int j = numCruzamentos; j>i; j--) {
+            for (int adjacente : adj[top[i]]) {
+
+                if (adjacente == top[j]) {
+                    dp[top[i]][top[j]] += 1;
+
+                } else {
+                    dp[top[i]][top[j]] += dp[adjacente][top[j]];
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i< numCruzamentos+1; i++) {
+        for (int j = 1; j<numCruzamentos+1; j++) {
+            if (i == j) continue;
+            int numCam = getNumCamiao(dp[i][j], numCamioes);
+            if (!(numCam <m1 || numCam >m2)) {
+                rotCam[numCam].emplace_back(pair<int, int>(i, j));
+            }
+        }
+    }
+
+    for (int i = m1; i<= m2; i++) {
+        printf("C%d", i);
+        for (pair<int, int> rot : rotCam[i]) {
+            printf(" %d,%d", rot.first, rot.second);
+        }
+        if (i!=m2) {
+            printf("\n");
+        }
+    }
+
+    //
+    // for (int i = 0; i< numCruzamentos+1; i++) {
+    //     for (int j = 0; j<numCruzamentos+1; j++) {
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
 
 
     return 0;
